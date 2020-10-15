17adcda365809b20225e32fc712157334f8cefb4
yuanlijian <yuanlijianvip@163.com>
2020-10-15 19:53:28 +0800

    后端请求响应的统一处理

--- Project/mini-tower/backend/src/app.ts
+++ Project/mini-tower/backend/src/app.ts
@@ -1,8 +1,10 @@
 import configs from './configs';
-import Koa from 'koa';
+import Koa, {Context, Next} from 'koa';
 import {bootstrapControllers} from 'koa-ts-controllers';
 import KoaRouter from 'koa-router';
+import koaBodyParser from 'koa-bodyparser';
 import path from 'path';
+import Boom from '@hapi/Boom';
 
 (async () => {
 
@@ -17,9 +19,36 @@ import path from 'path';
       versions: [1],
       controllers: [
           path.resolve( __dirname, 'controllers/**/*' )
-      ]
+      ],
+      errorHandler: async (err: any, ctx: Context) => {
+        console.log(err);
+
+        let status = 500;
+        let body: any = {
+            statusCode: status,
+            error: "Internal Server error",
+            message: "An internal server error occurred"
+        }
+
+        if (err.output) {
+            status = err.output.statusCode;
+            body = {...err.output.payload};
+            if (err.data) {
+                body.errorDetails = err.data;
+            }
+        }
+
+        ctx.status = status;
+        ctx.body = body;
+
+    }
   });
 
+  router.all('*', async ctx => {
+      throw Boom.notFound('没有该路由');
+  });
+
+  app.use( koaBodyParser() );
   app.use( router.routes() );
 
   app.listen( configs.server.port, configs.server.host, () => {
--- Project/mini-tower/backend/src/controllers/Test.ts
+++ Project/mini-tower/backend/src/controllers/Test.ts
@@ -1,15 +1,71 @@
 
 import {
+  Body,
   Controller,
-  Get
+  Get,
+  Header,
+  Params,
+  Post,
+  Query
 } from 'koa-ts-controllers';
+import {IsNumberString, IsNotEmpty} from 'class-validator';
+import Boom from '@hapi/Boom';
 
+class GetUsersQuery {
+
+  @IsNumberString()
+  page: number;
+
+}
 
 @Controller('/test')
 class TestController {
   @Get('/hello')
-  async hello() {
+  async hello(a: any) {
+    console.log(a.b);
     return 'Hello Test!';
+  }
+
+  @Get('/user/:id')
+  async getUser(
+    @Params('id') id: number
+  ) {
+    
+    return '当前params中用户id是：' + id;
+    // http://localhost:8080/api/v1/test/user/112
+  }
+
+  @Get('/user')
+  async getUser2(
+    @Query() q: {id: number}
+  ) {
+    return '当前params中用户id是：' + q.id;
+    // http://localhost:8080/api/v1/test/user?id=1
+  }
+
+  @Post('/user')
+  async postUser(
+    @Body() body: {
+      name: string,
+      password: string
+    },
+    @Header() h: any
+  ) {
+    console.log(body);
+    console.log(h);
+    return `当前提交的数据是：${JSON.stringify(body)}`;
+  }
+
+  @Get('/users')
+  async getUsers(
+    @Query() q: GetUsersQuery
+  ) {
+
+    // 业务逻辑出现了一些错误，比如用户不存在，用户名已经被注册了
+    // if (true) { // 用户名已经被注册了
+    //     throw Boom.notFound('注册失败', '用户已经被注册了');
+    // }
+    return '传过来的query:'+JSON.stringify(q);
   }
 
 }
