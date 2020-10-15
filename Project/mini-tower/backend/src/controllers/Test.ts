1e6d7e833980f84b26c0b493f2b9201821045d54
yuanlijian <yuanlijianvip@163.com>
2020-10-15 17:07:00 +0800

    后端koa-ts-controller使用

---
+++ Project/mini-tower/backend/src/controllers/Test.ts
@@ -0,0 +1,15 @@
+
+import {
+  Controller,
+  Get
+} from 'koa-ts-controllers';
+
+
+@Controller('/test')
+class TestController {
+  @Get('/hello')
+  async hello() {
+    return 'Hello Test!';
+  }
+
+}
