44b84949e077630e04e948a40e0eb7d5d5881956
yuanlijian <yuanlijianvip@163.com>
2020-12-09 08:28:48 +0800

    jsx useState hook

--- React-TypeScript/ts-with-react/src/App.tsx
+++ React-TypeScript/ts-with-react/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import Hello from './components/Hello'
+import Hello from './components/Hello';
+import LikeButton from './components/LikeButton';
 import './App.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Hello message="Hello World 2"/>
+        <LikeButton />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ React-TypeScript/ts-with-react/src/components/LikeButton.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const LikeButton: React.FC = () => {
+  const [like, setLike] = useState(0)
+  const [on, setOn] = useState(true)
+  return (
+    <>
+    <button onClick={() => {setLike(like + 1)}}>
+      {like} 👍
+    </button>
+    <button onClick = {() => {setOn(!on)}}>
+      {on ? 'ON' : 'OFF'}
+    </button>
+    </>
+  )
+}
+
+export default LikeButton
