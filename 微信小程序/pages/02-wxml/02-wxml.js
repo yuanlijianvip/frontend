1167553c60c5821c025e88146e8cee6eeb608589
yuanlijian <yuanlijianvip@163.com>
2021-04-10 10:06:10 +0800

    微信小程序

---
+++ 微信小程序/pages/02-wxml/02-wxml.js
@@ -0,0 +1,74 @@
+// pages/02-wxml/02-wxml.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    motto: '好好学习，天天向上',
+    name: '呷哺',
+   food: [
+     {id: 1, name: '粥'},
+     {id: 2, name: '面条'},
+     {id: 3, name: '混沌'}
+   ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // this.setData({
+    //   name: ''
+    // })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
