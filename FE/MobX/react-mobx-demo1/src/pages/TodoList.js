67ad30f633451dcb6da41e1aeccb954bee78c0a5
yuanlijian <yuanlijianvip@163.com>
2021-07-22 09:41:02 +0800

    mobx

--- FE/MobX/react-mobx-demo1/src/pages/TodoList.js
+++ FE/MobX/react-mobx-demo1/src/pages/TodoList.js
@@ -3,9 +3,20 @@ import { observer } from "mobx-react";
 
 @observer
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      count: 0
+    }
+  }
+
   render() {
+    const {input, count} = this.state;
     return (
       <div>
+        <input type="text" value={input} onChange={e => this.setState({input: e.target.value})}/>
+        <button onClick={()=>this.setState({count: count+1})}>add click {count}</button>
         {this.props.todoStore.todos.map((todo) => (
           <Todo
             key={todo.id}
@@ -20,6 +31,11 @@ class TodoList extends Component {
 }
 
 export default TodoList;
+
+const Child = React.memo(props => {
+  console.log("child props");
+  return <div>Child</div>
+});
 
 const Todo = observer(({ todo, change }) => {
   return (
